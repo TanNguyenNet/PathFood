d3cdfd412a9ebb6e6f30eeecf2563e45a7b145c6
tannb <itnbtan@gmail.com>
2019-10-08 17:30:40 +0700

    addQuestion

---
+++ src/CV.Web/Areas/Admin/Controllers/Catalog/CatalogFunctionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV.Web.Areas.Admin.Controllers.Catalog
+{
+    public class CatalogFunctionController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update(string id)
+        {
+            ViewBag.catalogId = id;
+            return View();
+        }
+
+    }
+}
--- src/CV.Web/Extension/ApplicationServiceSetup.cs
+++ src/CV.Web/Extension/ApplicationServiceSetup.cs
@@ -1,8 +1,12 @@
 ﻿using CV.Service;
 using CV.Service.Blog;
+using CV.Service.Catalog;
+using CV.Service.FAQ;
 using CV.Service.Identity;
 using CV.Service.Interface;
 using CV.Service.Interface.Blog;
+using CV.Service.Interface.Catalog;
+using CV.Service.Interface.FAQ;
 using CV.Service.Interface.Identity;
 using CV.Service.Interface.Setting;
 using CV.Service.Setting;
@@ -29,6 +33,16 @@ namespace CV.Web.Extension
             services.AddTransient<IPostService, PostService>();
 
             services.AddTransient<ILanguageService, LanguageService>();
+
+            services.AddTransient<ICatalogFunctionService, CatalogFunctionService>();
+
+            services.AddTransient<ICatalogSectorService, CatalogSectorService>();
+
+            services.AddTransient<IProductService, ProductService>();
+
+            services.AddTransient<IGroupQuestionService, GroupQuestionService>();
+
+            services.AddTransient<IQuestionService, QuestionService>();
 
         }
     }
