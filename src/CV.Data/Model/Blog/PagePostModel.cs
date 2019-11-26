9f3594c830c498352776324c4516cd14a42ecfd0
NBT <itnbtan@gmail.com>
2019-11-26 19:51:58 +0700

    add model page blog

---
+++ src/CV.Data/Model/Blog/PagePostModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV.Data.Model.Blog
+{
+    public class PagePostModel
+    {
+
+        public IEnumerable<PostModel> MixPost { set; get; }
+
+        public IEnumerable<CategoryBlogModel> Category { set; get; }
+
+        public IEnumerable<PostModel> Posts { set; get; }
+
+        
+    }
+}
